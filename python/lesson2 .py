d7a401545c0e250e3201f4bfc6fe0aed4a09da2d
Cyb3rFake <74620864+Cyb3rFake@users.noreply.github.com>
2023-06-06 17:30:23 +0300

    Update lesson2 .py
    Не самое оптимизированная реализация. Но после добавления функции проверки пользовательского ввода, вполне себе работоспособна.
    
    Signed-off-by: Cyb3rFake <74620864+Cyb3rFake@users.noreply.github.com>

--- python/lesson2 .py
+++ python/lesson2 .py
@@ -4,18 +4,53 @@
 #3) Ввести второе число 
 #4) вывести результат 
 
-num_1 = float(input('>:'))
-num_2 = float(input('>:'))
-operation = input('>:')
 
-print(operation)
-if operation == '+':
-    result = num_1 + num_2 
-    print(int(result))
-elif operation == '-':
-    result = num_1 - num_2
-    print(int(result))
-else:
-    print('unknown operation')  
+operation_list = ['+', '-', '/', '*']
+
+
+def calculate(num_1, math_, num_2):
+    """Prints to console result"""
+
+    match math_:
+        case '+':
+            if '.' in (num_1 + num_2):
+                result = float(num_1).__add__(float(num_2))
+            else:
+                result = int(num_1).__add__(int(num_2))     
+            print(f'\n{num_1} {math_} {num_2} = {result}\n')
+        case '-':
+            if '.' in (num_1 + num_2):
+                result = float(num_1).__sub__(float(num_2))
+            else:
+                result = int(num_1).__sub__(int(num_2))
+            print(f'\n{num_1} {math_} {num_2} = {result}\n')
+        case '/':
+            result = float(num_1).__truediv__(float(num_2))
+            result = int(result) if str(result)[-1] == '0' else float(result)
+            print(f'\n{num_1} {math_} {num_2} = {result}\n')
+        case '*':
+            if '.' in (num_1 + num_2):
+                result = float(num_1).__mul__(float(num_2))
+            else:
+                result = int(num_1).__mul__(int(num_2))
+            print(f'\n{num_1} {math_} {num_2} = {result}\n')
+        case _:
+            print('unknown operation')
+
+
+def check_input_data(n1, opr, n2):
+    """Check incoming data"""
+    pass
+
+
+def run_calc():
+    num_1 = input('Input first number:')
+    operation = input('Input an math operation(+,-,/,*): ')
+    num_2 = input('Input second number:')
+    calculate(num_1, operation, num_2)
+
+
+run_calc()
+
 
 
